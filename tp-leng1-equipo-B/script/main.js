bd6cfdf108a8d1b9949c3872541d27ac267deca9
Juanciro <juanciroarzamendia@gmail.com>
2026-06-29 01:33:55 -0300

    feat, fix
    se agrega boton que muestra implementacion en proyectos.
    se cambia el estilo de las tarjetas de implementacion y la portada de los proyectos.
    se corrigen rutas de archivos de imagenes en el proyecto

---
+++ tp-leng1-equipo-B/script/main.js
@@ -0,0 +1,20 @@
+const btnadopcion = document.getElementById("btn-adopcion");
+const adopcion = document.getElementById("imp-adopcion");
+
+btnadopcion.addEventListener("click", () =>{
+    adopcion.classList.toggle("hidden")
+})
+
+let btncastracion = document.getElementById("btn-castracion");
+let castracion = document.getElementById("imp-castracion");
+
+btncastracion.addEventListener("click", () =>{
+    castracion.classList.toggle("hidden")
+})
+
+let btneducacion = document.getElementById("btn-educacion");
+let educacion = document.getElementById("imp-educacion");
+
+btneducacion.addEventListener("click", () =>{
+    educacion.classList.toggle("hidden")
+})
