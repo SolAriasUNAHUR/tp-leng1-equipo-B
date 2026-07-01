d76c21d05e1ae0fb5d0ccf6608af2548327062bb
SolAriasUNAHUR <mariasol.arias@estudiantes.unahur.edu.ar>
2026-07-01 11:23:04 -0300

    Correcciones generales

--- tp-leng1-equipo-B/script/main.js
+++ tp-leng1-equipo-B/script/main.js
@@ -5,15 +5,15 @@ btnadopcion.addEventListener("click", () =>{
     adopcion.classList.toggle("hidden")
 })
 
-let btncastracion = document.getElementById("btn-castracion");
-let castracion = document.getElementById("imp-castracion");
+const btncastracion = document.getElementById("btn-castracion");
+const castracion = document.getElementById("imp-castracion");
 
 btncastracion.addEventListener("click", () =>{
     castracion.classList.toggle("hidden")
 })
 
-let btneducacion = document.getElementById("btn-educacion");
-let educacion = document.getElementById("imp-educacion");
+const btneducacion = document.getElementById("btn-educacion");
+const educacion = document.getElementById("imp-educacion");
 
 btneducacion.addEventListener("click", () =>{
     educacion.classList.toggle("hidden")
